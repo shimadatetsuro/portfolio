d6fcdb855b5083faeb30afbb3a1b9c05253ecdca
shimadatetsuro <129487430+shimadatetsuro@users.noreply.github.com>
2023-04-03 12:58:23 +0900

    Add files via upload

---
+++ portfolio1/page-portfolio1.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="はじめての〇〇。ここには検索エンジンに自分のサイトの概要を説明するエリアです">
+    <link href="">
+    <!-- GoogleFontsここから -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@500&family=Piazzolla:opsz,wght@8..30,300&display=swap" rel="stylesheet">
+    <!-- GoogleFontsここまで -->
+    <!-- CSSここから -->
+    <link rel="stylesheet" href="<?php echo get_theme_file_uri('css/reset.css') ?>">
+    <link rel="stylesheet" href="<?php echo get_theme_file_uri('css/style.css') ?>">
+    <!-- CSSここまで -->
+    <link rel="icon" href="<?php echo get_theme_file_uri('images/HatchfulExport-All/favicon.png') ?>">
+<title>猫の紹介サイト</title>
+</head>
+<body>
+    <header>
+        <h1>
+            <span class="logo-main">
+                CAT
+            </span>
+            <br>-地域猫について-
+        </h1>
+        <p class="header-img"></p>
+        <nav class="gnav">
+            <ul class="wrap">
+                <li><a href="index.html">Home</a></li>
+                <li><a href="blog.html">Blog</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <section class="bg-gray">
+        <div class="wrap">
+            <div class="flex-box">
+                <div class="flex-l">
+                    <h2 class="title"><span>地</span>域猫活動とは？</h2>
+                    <p>「飼い主のいない猫でも、生きているんだから何かをしてあげたい」と思っている人はたくさんいます。一方、「飼い主のいない猫は迷惑だ」と思っている人もたくさんいます。地域には、猫が好きな人、無関心な人、嫌いあるいは苦手（動物アレルギー体質等も含む）な人が混在して住んでいます。</p>
+                    <p>望まれない命は作らない、作らせない。でも、生まれてきた命はできるだけ長生きさせてあげたい･･･。こんな気持ちを持つ人たちが、猫と地域の共生を目指して、「地域猫活動」を進めるケースが増えてきていますが、「地域猫活動」が、ただの「エサやり」になってしまうと、地域社会に新たな問題を起こしてしまうことになりかねません。</p>
+                    <p>エサをあげることが悪いことではありません、またエサをあげないことが解決になるわけではありません。</p>
+                    <p>地域の中で意見交換をして、出来るだけ多くの合意を得て「地域猫活動」を進めていただければと思います。</p>
+                </div>
+                <div class="flex-r">
+                    <h2 class="title"><span>地</span>域猫の特長</h2>
+                    <ul>
+                        <li>
+                            「地域猫」とは、一言でいうと「地域で管理されている野良猫」のことです。特定の飼い主がおらずお外で生活するという点では野良猫と同じですが、地域住民の方々の合意のもと特定のごはん場やトイレを設置し皆で適切に面倒を見ていくという点で大きく異なります。
+                        </li>
+                        <li>
+                            地域猫は不妊手術を受け、目印のために耳先をカットしたりピアスをつけたりしています。もしあなたが道端で耳先をカットされた猫に出会ったならば、その子はきっとその付近で可愛がられている地域猫さんでしょう。
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <small>
+            &copy;2023 Shimada.com
+        </small>
+    </footer>
+</body>
+</html>
